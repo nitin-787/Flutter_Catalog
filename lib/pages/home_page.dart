ae3cff366cdd74c9ed8920dd209e3adde331de55
Nitin Sharma <nitinsharmaa787@gmail.com>
2022-06-14 20:14:06 +0530

    Sucessfully displayed the json data in our app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,18 +19,19 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     loadData();
+    setState(() {});
   }
 
   loadData() async {
     var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
     var decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
-    print(productsData);
+    CatalogModel.items =
+        List.from(productsData).map((item) => Item.fromMap(item)).toList();
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog"),
@@ -38,10 +39,10 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummyList.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: dummyList[index],
+              item: CatalogModel.items[index],
             );
           },
         ),
